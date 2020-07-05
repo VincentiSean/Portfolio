6dcbf6d72abef69743008b5b529cb6d3966b2f36
Sean Vincenti <sjv18@pitt.edu>
2020-07-04 22:03:10 -0400

    Various additions
    Implemented portfolio page design and about page design

--- clickHandler.js
+++ clickHandler.js
@@ -1,5 +1,6 @@
 var homeSection = document.getElementById('content');
 var portfolioSection = document.getElementById('portfolioSection');
+var aboutSection = document.getElementById('aboutMeSection');
 
 var heroTitle = document.getElementById('heroTitle');
 var heroTopText = document.getElementById('heroTopText');
@@ -20,6 +21,7 @@ function goto(location) {
 function goHome() {
     homeSection.style.display = 'flex';
     portfolioSection.style.display = 'none';
+    aboutSection.style.display = 'none';
 
     heroTopText.innerHTML = 'Web&nbsp;&nbsp;&nbsp;&&nbsp;&nbsp;&nbsp;Software';
     heroTopText.style.marginBottom = '0px';
@@ -33,19 +35,21 @@ function goHome() {
 function goAbout() {
     homeSection.style.display = 'none';
     portfolioSection.style.display = 'none';
+    aboutSection.style.display = 'block';
 
-    heroTopText.innerHTML = '';
-    heroTopText.style.marginBottom = '30px';
+    contactButton.style.display = 'none';
+    heroTopText.innerHTML = 'Find out more';
+    heroTopText.style.marginBottom = '0px';
     heroBottomText.innerHTML = 'About Me';
     header.style.height = '75vh';
     document.styleSheets[6].addRule('header::before', 'height: 75%;');
     heroTitle.style.marginTop = '40px';
-    contactButton.style.display = 'none';
 }
 
 function goPort() {
     homeSection.style.display = 'none';
     portfolioSection.style.display = 'block';
+    aboutSection.style.display = 'none';
 
     heroTopText.innerHTML = 'View my previous<span id="heroBottomText"></span>';
     heroTopText.style.marginBottom = '-30px';
